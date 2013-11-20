9ecd9b7aa417b38446643518f6defb85a4d76a4c
Tom Opgenorth <tom@opgenorth.net>
2013-11-20 12:41:45 +0100

    Tweak some of the steps in the scenario.

--- TaskyPro/features/add_new_task.feature
+++ TaskyPro/features/add_new_task.feature
@@ -2,9 +2,8 @@ Feature: Add New Task
 
   Scenario: Add a new Task
     Given I am on the Task Details screen
-    When I name the Task "Learn Objective-C"
-    When notes are "It helps with creating Xamarin.iOS bindings if you know Objective-C"
+    When I add the "Learn Objective-C" task
     And tap the Save button
 
     Then I should be on the TaskyPro screen
-    And I should see "Learn Objective-C" in the list
+    And I should see the "Learn Objective-C" task in the list
--- TaskyPro/features/step_definitions/task_details_steps.rb
+++ TaskyPro/features/step_definitions/task_details_steps.rb
@@ -1,18 +1,18 @@
 Given(/^I am on the Task Details screen$/) do
-
   tasks_page = page(TaskyProScreen).await
-
   @current_page = tasks_page.tap_add_task_button
 
 end
 
-When(/^I name the Task "(.*?)"$/) do |arg1|
-  pending # express the regexp above with the code you wish you had
+When(/^I add the "(.*?)" task$/) do |task_name|
+
+  new_task = TASKS[task_name.downcase.to_sym]
+
+  puts new_task
+
+  @current_page.add_new_task(new_task)
 end
 
-When(/^notes are "(.*?)"$/) do |arg1|
-  pending # express the regexp above with the code you wish you had
-end
 
 When(/^tap the Save button$/) do
   pending # express the regexp above with the code you wish you had
@@ -22,6 +22,6 @@ Then(/^I should be on the TaskyPro screen$/) do
   pending # express the regexp above with the code you wish you had
 end
 
-Then(/^I should see "(.*?)" in the list$/) do |arg1|
+Then(/^I should see the "(.*?)" task in the list$/) do |task_name|
   pending # express the regexp above with the code you wish you had
 end
